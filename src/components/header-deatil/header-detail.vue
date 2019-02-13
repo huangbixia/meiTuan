5a14753c6cfeff65df5b44d38f36ce69f226616a
黄碧霞 <huangbx@meicloud.com>
2019-02-13 20:57:49 +0800

    <ADD>header-detail

---
+++ src/components/header-deatil/header-detail.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="header-detail">
+    <div class="detail-wrapper clear-fix">
+      <div class="detail-main">
+        <h1 class="name">{{seller.name}}</h1>
+        <div class="star-wrapper">
+          <star :size="48" :score="seller.score"></star>
+        </div>
+        <div class="title">
+          <div class="line"></div>
+          <div class="text">优惠信息</div>
+          <div class="line"></div>
+        </div>
+        <ul v-if="seller.supports" class="supports">
+          <li class="support-item" v-for="(item, index) in seller.supports" :key="item.id">
+            <support-ico :size="2" :type="seller.supports[index].type"></support-ico>
+            <span class="text">{{sellers.supports[index].description}}</span>
+          </li>
+        </ul>
+        <div class="title">
+          <div class="line"></div>
+          <div class="text">商家公告</div>
+          <div class="line"></div>
+        </div>
+        <div class="bulletin">
+            <p class="content">{{seller.bulletin}}></p>
+        </div>
+      </div>
+    </div>
+    <div class="detail-close" @click="hide">
+        <i class="icon-close"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+import popupMixin from 'common/mixins/popup'
+import Star from 'components/star/star'
+import SupportIco from 'components/support-ico/support-ico'
+
+export default {
+    name: 'header-detail',
+    mixins: [popupMixin],
+    props: {
+        
+    }
+}
+</script>>
