3c8d0cf90d59d8b0a412f1c279e2bc51e84709f0
黄碧霞 <huangbx@meicloud.com>
2019-05-21 07:11:14 +0800

    <Change>修改配置文件

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import { get } from './helper'
 
-const getSeller = get('/api/seller')
-const getGoods = get('/api/goods')
-const getRatings = get('/api/ratings')
+const getSeller = get('api/seller')
+const getGoods = get('api/goods')
+const getRatings = get('api/ratings')
 
 // 导出方法
 export {
